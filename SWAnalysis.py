947a2d5e9c363b32722c56d07555d529dae4c26a
Marco Carnaghi <125691581+MarcoCarnaghiCoding@users.noreply.github.com>
2023-09-08 00:12:06 -0300

    Add files via upload

--- SWAnalysis.py
+++ SWAnalysis.py
@@ -159,7 +159,7 @@ def SWAnalysisPage():
                 angles += angles[:1]
 
                 # Initialize the spider plot
-                fig = plt.figure(facecolor='black')
+                fig = plt.figure(facecolor='#0e1117')
                 ax = fig.add_subplot(111, polar=True)
 
                 # Draw one axe per variable + add labels
@@ -182,7 +182,7 @@ def SWAnalysisPage():
                 ax.yaxis.grid(color='white', alpha=0.5)
 
                 # Set the background color of the figure
-                fig.set_facecolor('black')
+                fig.set_facecolor('#0e1117')
 
                 # Show the graph
                 st.pyplot(fig)
